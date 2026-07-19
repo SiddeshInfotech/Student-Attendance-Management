38dae175d51407e5eddebb475ddc518320998aaf
hemangisuryawanshi1209 <hemangipatil7812@gmail.com>
2026-07-19 18:29:59 +0530

    Update students.jsx

--- src/data/students.jsx
+++ src/data/students.jsx
@@ -1,25 +1,66 @@
 const defaultStudents = [
   {
     id: 1,
-    fullName: "Rahul Patil",
+    fullName: "Hemangi Suryawanshi",
     rollNo: "101",
-    className: "BCA FY",
-    email: "rahul@gmail.com",
-    username: "rahul",
+    className: "Computer Engineering",
+    email: "hemangi@gmail.com",
+    username: "hemangi",
     password: "123456",
+
     attendance: [
       {
         date: "2026-07-01",
-        subject: "Java",
+        subject: "Java Programming",
         status: "Present",
       },
       {
         date: "2026-07-02",
-        subject: "React",
+        subject: "Python",
+        status: "Present",
+      },
+      {
+        date: "2026-07-03",
+        subject: "React JS",
         status: "Absent",
       },
-    ],
-  },
+      {
+        date: "2026-07-04",
+        subject: "HTML",
+        status: "Present",
+      },
+      {
+        date: "2026-07-05",
+        subject: "CSS",
+        status: "Present",
+      },
+      {
+        date: "2026-07-06",
+        subject: "JavaScript",
+        status: "Absent",
+      },
+      {
+        date: "2026-07-07",
+        subject: "Software Engineering",
+        status: "Present",
+      },
+      {
+        date: "2026-07-08",
+        subject: "Database Management",
+        status: "Present",
+      },
+      {
+        date: "2026-07-09",
+        subject: "Computer Network",
+        status: "Present",
+      },
+      {
+        date: "2026-07-10",
+        subject: "Python Practical",
+        status: "Absent",
+      }
+    ]
+  }
 ];
 
 export default defaultStudents;
